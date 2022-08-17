226aab92dbdd344435e2c5ad5b40e9c196ef2ffe
Kartikey Sharma <78343656+kartikey81@users.noreply.github.com>
2022-08-17 12:03:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+@app.route('/aboutus')
+def aboutus():
+  return render_template('aboutus.html')
+@app.route('/minorproject')
+def majorproject():
+  return render_template('minorproject.html')
+app.run()
