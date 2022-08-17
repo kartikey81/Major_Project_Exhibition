67a506468abfe122a2e5847c1a164ef58df1e540
Kartikey Sharma <78343656+kartikey81@users.noreply.github.com>
2022-08-17 12:09:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def aboutus():
 @app.route('/minorproject')
 def majorproject():
   return render_template('minorproject.html')
-if "__name__" == __main__:
+if __name__ == "__main__":
   app.run(debug=True)
